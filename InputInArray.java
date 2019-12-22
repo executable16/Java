7798704f5876798e32fbba09ddef5bebbd838cad
Atul Malakar <atulmalakar15@gmail.com>
2019-12-22 12:22:36 +0530

    Create InputInArray.java

---
+++ InputInArray.java
@@ -0,0 +1,19 @@
+import java.lang.Math;
+import java.util.*;
+import java.io.*;
+class hello{
+	public static void main(String[] args) throws IOException{ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		String s[] = br.readLine().trim().split(" ");	
+		int a[] = new int[n];
+
+		for(int i=0;i<n;i++){
+			a[i] = Integer.parseInt(s[i]);
+		}
+		for(int i : a){
+			System.out.print(i + " ");
+		}
+	}
+}
